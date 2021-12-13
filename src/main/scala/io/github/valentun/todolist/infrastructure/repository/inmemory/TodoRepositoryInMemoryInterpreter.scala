f65f232ee3d50648fdd7bcb7ea06564be4e3d786
ValentunSergeev <karbon4ic@gmail.com>
2021-12-13 21:01:30 +0400

    Make in-memory repository implementations pure

--- src/main/scala/io/github/valentun/todolist/infrastructure/repository/inmemory/TodoRepositoryInMemoryInterpreter.scala
+++ src/main/scala/io/github/valentun/todolist/infrastructure/repository/inmemory/TodoRepositoryInMemoryInterpreter.scala
@@ -1,6 +1,7 @@
 package io.github.valentun.todolist.infrastructure.repository.inmemory
 
-import cats._
+import cats.Applicative
+import cats.effect.Async
 import cats.implicits._
 import io.github.valentun.todolist.domain.todos.{Todo, TodoRepositoryAlgebra}
 
@@ -12,29 +13,43 @@ class TodoRepositoryInMemoryInterpreter[F[_]: Applicative] extends TodoRepositor
 
   private val random = new Random
 
-  override def create(todo: Todo, userId: Long): F[Todo] = {
-    val id = random.nextLong()
-    val toSave = todo.copy(id = id.some, userId = userId.some)
-    cache += (id -> toSave)
-    toSave.pure[F]
-  }
+  override def create(todo: Todo, userId: Long): F[Todo] = Applicative[F]
+    .pure((todo, userId))
+    .map { case (todo, userId) =>
+      val id = random.nextLong()
+      val toSave = todo.copy(id = id.some, userId = userId.some)
+      cache += (id -> toSave)
 
-  override def update(todo: Todo): F[Option[Todo]] = todo.id.traverse { id =>
-    cache.update(id, todo)
-    todo.pure[F]
-  }
+      toSave
+    }
 
-  override def get(id: Long): F[Option[Todo]] = cache.get(id).pure[F]
+  override def update(todo: Todo): F[Option[Todo]] = Applicative[F]
+    .pure(todo)
+    .map { todo =>
+      todo.id.map { id =>
+        cache.update(id, todo)
 
-  override def delete(id: Long): F[Option[Todo]] = cache.remove(id).pure[F]
+        todo
+      }
+    }
 
-  override def findByUser(userId: Long): F[List[Todo]] =
-    cache.values
-      .filter(todo => todo.userId.get == userId)
-      .toList
-      .pure[F]
+  override def get(id: Long): F[Option[Todo]] = Applicative[F]
+    .pure(id)
+    .map(cache.get)
+
+  override def delete(id: Long): F[Option[Todo]] = Applicative[F]
+    .pure(id)
+    .map(cache.remove)
+
+  override def findByUser(userId: Long): F[List[Todo]] = Applicative[F]
+    .pure(userId)
+    .map { userId =>
+      cache.values
+        .filter(todo => todo.userId.get == userId)
+        .toList
+    }
 }
 
 object TodoRepositoryInMemoryInterpreter {
-  def apply[F[_]: Applicative]() = new TodoRepositoryInMemoryInterpreter[F]()
+  def apply[F[_]: Async]() = new TodoRepositoryInMemoryInterpreter[F]()
 }
